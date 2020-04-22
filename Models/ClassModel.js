7b0139d90ba69a83425a2e3f0cb74384f756c2fe
Ubuntu <daiki_itoh@Web-Dev-CS3613.zhmnxgy25emelmc3xnndanlwra.bx.internal.cloudapp.net>
2020-04-22 22:48:21 +0000

    Modify db attribute email to instructor 5:47pm 4/22

--- Models/ClassModel.js
+++ Models/ClassModel.js
@@ -2,20 +2,20 @@ class ClassModel {
     constructor (DAO) {
         this.DAO = DAO
     }
-  
+
     async createTable () {
         const sql = `
             CREATE TABLE IF NOT EXISTS Class (
             classID TEXT PRIMARY KEY,
             name TEXT,
-            email TEXT REFERENCES Users (email)
+            instructor TEXT REFERENCES Users (email)
             ON DELETE CASCADE
         )`
         return await this.DAO.run(sql);
     }
 
-    async addClass (classID, name) {
-        const sql = `INSERT INTO Class (classID, name, email) VALUES (?, ?, ?)`;
+    async addClass (classID, name, email) {
+        const sql = `INSERT INTO Class (classID, name, instructor) VALUES (?, ?, ?)`;
         // classID needs to be unique so this will throw an exception if we 
         // attempt to add a class that already exists
         await this.DAO.run(sql, [classID, name, email]);
