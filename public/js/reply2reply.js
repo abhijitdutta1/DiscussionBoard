34c6de5c21a3192857734d7aa1a2a28a401ab8d3
root <root@web-dev.1mnlm1evixhelkwvxioebfnyvb.jx.internal.cloudapp.net>
2020-05-08 11:36:45 +0000

    Final product

--- public/js/reply2reply.js
+++ public/js/reply2reply.js
@@ -37,35 +37,6 @@ async function postReply() {
     }
 }
 
-function getquestion() {
-    let qid = window.location.href.split('/')[6]
-    let ReplyID = window.location.href.split('/')[7]
-    fetch(`http://40.84.158.14/discussion-list/${qid}/${ReplyID}/`, {
-        method: 'GET'
-    }).then( res => {
-        return res.json();
-    }).then( data => {
-        local_items2 = data.replies;
-        //  ********
-        const template = document.getElementById('template');
-        let list_elt = document.getElementById('replies-lst');
-        list_elt.innerHTML = '';
-            
-        for (let i = 0; i < local_items2.length; ++i) {
-            let new_li = document.importNode(template.content, true);
-
-            // get replies
-            new_li.querySelector('.name').textContent = local_items2[i].user;
-            new_li.querySelector('.date').textContent = local_items2[i].date;
-            new_li.querySelector('.contents').textContent = local_items2[i].Reply;
-            list_elt.appendChild(new_li);
-        }
-        //  ********
-    }).catch( err => {
-        console.log(err);
-    });
-}
-
 function getReplies() {
     let qid = window.location.href.split('/')[6]
     let ReplyID = window.location.href.split('/')[7]
